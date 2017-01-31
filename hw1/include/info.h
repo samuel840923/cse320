df41a1f0b3b7cf374fcd3f9d5128d595cf59fd03
Samuel Chen <samuel.chen@stonybrook.edu>
2017-01-31 13:50:50 -0500

    fix info file

--- hw1/include/info.h
+++ hw1/include/info.h
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef INFO_H
+#ifdef INFO
 #define info_shift(shifted) fprintf(stderr,"%s%s\n","INFOL shifted alphabet: ",shifted)
 #define info_amount(n)  fprintf(stderr,"%s%d\n", "INFO: shift amount: ",n);
 #define info_input(input) fprintf(stderr,"%s%s\n", "INFO: input file: ",input);
 #define info_out(output) fprintf(stderr,"%s%s\n", "INFO: output file: ",output);
 #define info_op(op) fprintf(stderr,"%s%s\n", "INFO: operation: ",op);
 #else
- #define info_shift(shifted) fprintf(stderr,"%s%s\n","INFOL shifted alphabet: ",shifted)
-#define info_amount(n)  fprintf(stderr,"%s%d\n", "INFO: shift amount: ",n);
-#define info_input(input) fprintf(stderr,"%s%s\n", "INFO: input file: ",input);
-#define info_out(output) fprintf(stderr,"%s%s\n", "INFO: output file: ",output);
-#define info_op(op) fprintf(stderr,"%s%s\n", "INFO: operation: ",op);
+ #define info_shift(shifted)
+#define info_amount(n)
+#define info_input(input)
+#define info_out(output)
+#define info_op(op)
 #endif
 #endif
